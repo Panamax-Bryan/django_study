7bf94c866a41a6bbecb25aadf7b22a81195ba47b
hyungsub <koreanronin@gmail.com>
2021-04-24 22:47:41 +0900

    Add: Assignment2에 관한 urls.py 경로 추가

--- practice/urls.py
+++ practice/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Assign1View
+from .views import Assign1View, Assign2View
 
 urlpatterns = [
     path('/assign1', Assign1View.as_view()),
+    path('/assign2', Assign2View.as_view()),
 ]
--- practice/views.py
+++ practice/views.py
@@ -18,14 +18,14 @@ class Assign1View(View):
         return JsonResponse({'RESULT' : sub_list}, status=200)
 
 
-#class assignment_2(View):
-#    def get(self, request):
-#        a = MainCategory.objects.get(name='눈 건강')
-#        
-#        sub_list = []
-#        for i in SubCategory.objects.filter(main_category=a):
-#            sub_list.append({
-#                'name' : i.name,
-#                'main_category_id' : i.main_category_id
-#                })
-#        return JsonResponse({'RESULT' : sub_list}, status=200)
+class Assign2View(View):
+    def get(self, request):
+        a = MainCategory.objects.get(name='눈 건강')
+        
+        sub_list = []
+        for i in SubCategory.objects.filter(main_category=a):
+            sub_list.append({
+                'name' : i.name,
+                'main_category_id' : i.main_category_id
+                })
+        return JsonResponse({'RESULT' : sub_list}, status=200)
